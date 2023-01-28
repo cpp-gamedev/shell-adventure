cc1fffabc9fbc6854dfe0c2055c2cfb7b7df8843
aleokdev <aleok.inf@gmail.com>
2023-01-28 18:00:24 +0100

    Basic directory navigation & cmd system

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,97 @@
 pub mod world;
 
-use std::{collections::HashMap, io::Write};
+use std::{collections::HashMap, io::Write, path};
 
-use crate::world::Machine;
+use itertools::Itertools;
+
+use crate::world::{DirEntry, Directory, File, Machine, Path};
 
 fn main() -> Result<(), std::io::Error> {
     println!("Hello, world!");
     let stdin = std::io::stdin();
     let mut world = Machine {
-        cwd: "/".to_string(),
-        dir_tree: world::Directory {
-            files: vec![],
-            dirs: vec![],
+        cwd: Path::root(),
+        root_dir: world::Directory {
+            files: HashMap::from_iter(
+                [(
+                    "README.txt".to_owned(),
+                    File {
+                        data: "Hello World!".to_owned(),
+                        is_executable: false,
+                    },
+                )]
+                .into_iter(),
+            ),
+            dirs: HashMap::from_iter(
+                [(
+                    "test".to_owned(),
+                    Directory {
+                        dirs: Default::default(),
+                        files: Default::default(),
+                        is_writable: false,
+                    },
+                )]
+                .into_iter(),
+            ),
             is_writable: false,
         },
     };
 
     loop {
-        print!("> ");
+        print!("{} > ", world.cwd.to_string());
         std::io::stdout().flush()?;
         let mut query = String::new();
         stdin.read_line(&mut query)?;
-        let query = Query::new(&query);
+        let Some(query) = Query::new(&query) else { continue; };
+
+        match (query.executable.as_ref(), query.params.as_slice()) {
+            ("ls", _) => {
+                let cwd = match world.traverse(world.cwd.clone()).unwrap() {
+                    world::DirEntry::Directory(dir) => dir,
+                    world::DirEntry::File(_) => unreachable!(),
+                };
+                let result = cwd
+                    .dirs
+                    .keys()
+                    .map(String::as_ref)
+                    .chain(cwd.files.keys().map(String::as_ref))
+                    .intersperse("\t")
+                    .collect::<String>();
+                println!("{}", result);
+            }
+            ("cwd", _) => {
+                println!("{}", world.cwd.to_string());
+            }
+            ("cd", [path]) => {
+                let path = world.cwd.clone() + Path::parse(path);
+                let is_valid = matches!(world.traverse(path.clone()), Some(DirEntry::Directory(_)));
+
+                if is_valid {
+                    world.cwd = path;
+                } else {
+                    println!("No such path: \"{}\"", path.to_string());
+                }
+            }
+            ("cd", [..]) => {
+                println!("Invalid number of parameters.\nExpected usage: cd <dir>");
+            }
+            _ => {
+                println!("Unrecognized executable or command");
+            }
+        }
     }
 }
 
 pub struct Query {
-    contents: String,
+    executable: String,
+    params: Vec<String>,
 }
 
 impl Query {
-    pub fn new(contents: &str) -> Self {
-        Self {
-            contents: contents.trim().to_string(),
-        }
+    pub fn new(contents: &str) -> Option<Self> {
+        let mut parts = contents.split_whitespace();
+        let executable = parts.next()?.to_owned();
+        let params = parts.map(ToOwned::to_owned).collect();
+        Some(Self { executable, params })
     }
 }
--- src/world.rs
+++ src/world.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use crate::Query;
+use itertools::Itertools;
 
 pub struct File {
     pub data: String,
@@ -8,12 +8,124 @@ pub struct File {
 }
 
 pub struct Directory {
-    pub files: Vec<File>,
-    pub dirs: Vec<Directory>,
+    pub files: HashMap<String, File>,
+    pub dirs: HashMap<String, Directory>,
     pub is_writable: bool,
 }
 
+pub enum DirEntry<'f> {
+    File(&'f File),
+    Directory(&'f Directory),
+}
+
 pub struct Machine {
-    pub cwd: String,
-    pub dir_tree: Directory,
+    pub cwd: Path,
+    pub root_dir: Directory,
+}
+
+// TODO: Borrowed version
+// TODO: Special components (.., .)
+#[derive(Clone)]
+pub struct Path {
+    components: Vec<String>,
+    is_absolute: bool,
+}
+
+impl Path {
+    pub fn root() -> Self {
+        Self {
+            components: vec![],
+            is_absolute: true,
+        }
+    }
+
+    pub fn parse(str: &str) -> Self {
+        let (str, is_absolute) = if str.starts_with('/') {
+            (&str[1..], true)
+        } else {
+            (str, false)
+        };
+        let mut components: Vec<String> = str.split('/').map(ToOwned::to_owned).collect();
+        if components.last().map_or(false, String::is_empty) {
+            components.pop();
+        }
+
+        Self {
+            components,
+            is_absolute,
+        }
+    }
+
+    pub fn is_absolute(&self) -> bool {
+        self.is_absolute
+    }
+
+    pub fn is_relative(&self) -> bool {
+        !self.is_absolute
+    }
+}
+
+// TODO: Same semantics as String add (Owned + Borrowed)
+// TODO: impl AddAssign
+impl std::ops::Add for Path {
+    type Output = Path;
+
+    fn add(mut self, mut rhs: Self) -> Self::Output {
+        if rhs.is_absolute() {
+            rhs
+        } else {
+            self.components.extend(rhs.components.drain(..));
+            self
+        }
+    }
+}
+
+// TODO: impl std::fmt::Display for Path
+impl ToString for Path {
+    fn to_string(&self) -> String {
+        let relative_result = self
+            .components
+            .iter()
+            .map(String::as_ref)
+            .intersperse("/")
+            .collect::<String>();
+        if self.is_absolute {
+            "/".to_owned() + relative_result.as_ref()
+        } else {
+            relative_result
+        }
+    }
+}
+
+impl Machine {
+    // TODO: Take borrowed version of path instead
+    // TODO: Return a Result instead of an Option. Use thiserror for the error type
+    pub fn traverse(&self, path: Path) -> Option<DirEntry> {
+        let path = if path.is_relative() {
+            self.cwd.clone() + path
+        } else {
+            path
+        };
+
+        let mut current_dir = &self.root_dir;
+        for (is_last, component) in path
+            .components
+            .iter()
+            .enumerate()
+            .map(|(idx, x)| (idx == path.components.len() - 1, x))
+        {
+            if is_last {
+                if current_dir.files.contains_key(component) {
+                    return Some(DirEntry::File(&current_dir.files[component]));
+                }
+            }
+            if current_dir.dirs.contains_key(component) {
+                current_dir = &current_dir.dirs[component];
+            } else {
+                return None;
+            }
+        }
+
+        Some(DirEntry::Directory(current_dir))
+    }
 }
